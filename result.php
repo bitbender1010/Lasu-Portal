30f1dc82c81fa861bad2c59df521eb991f553256
Dammie <bitbender1010@gmail.com>
2020-03-05 21:54:53 +0100

    update php

--- result.php
+++ result.php
@@ -1,8 +1,35 @@
 <?php
-if(isset($_POST['login'])) {
-    echo 'Hii'
+ if(isset($_POST['submit'])) {
+
+    $name = name;
+    $level = level;
+    $matric: matric;
+
+    $course1 = course1;
+    $course2 = course2;
+    $course3 = course3;
+
+    $score1 = score1;
+    $score2 = score2;
+    $score3 = score3;
+
+    echo $name;
+    echo $level;
+    echo $matric;
+
+    echo "<br>"
+
+    echo $course1;
+    echo $score1;
+    
+    echo "<br>"
+
+    echo $course2;
+    echo $score2;
+
+    echo "<br>"
+
+    echo $course3;
+    echo $score3;
+
 }
-
-
-
-?>
